d9cf8e47b34c0d500ac3f1376896f48cb164976c
Mayankt-21 <92510723+Mayankt-21@users.noreply.github.com>
2024-08-24 17:56:49 +0530

    upload to github

---
+++ backend/controllers/getData.js
@@ -0,0 +1,84 @@
+const {spawn} = require("child_process");
+const path = require('path'); 
+
+async function callAI(imagePath) {
+    return new Promise((resolve, reject) => {
+      const scriptPath = path.join(__dirname, '../AI/AI.py');
+      const pythonProcess = spawn('python', [scriptPath, imagePath]);
+  
+      let outputData = '';
+  
+      pythonProcess.stderr.on('data', (err) => {
+        const errString = err.toString();
+        
+        if (!errString.includes('Using cache found in') &&
+            !errString.includes('YOLOv5') &&
+            !errString.includes('Fusing layers...') &&
+            !errString.includes('YOLOv5l summary:') &&
+            !errString.includes('Adding AutoShape...')) {
+            console.error('Error from Python script:', errString);
+            reject(new Error('Error running Python script'));
+        }
+      });
+  
+      pythonProcess.stdout.on('data', (data) => {
+        outputData += data.toString();
+        
+      });
+
+      pythonProcess.on('close', (code) => {
+        if (code === 0) {
+            try {
+              const jsonData = JSON.parse(outputData);
+              
+              const vehicleCount = jsonData.vehicle_count;
+              
+              resolve(vehicleCount);
+            } catch (error) {
+              console.error('Error parsing vehicle count:', error);
+              reject(new Error('Invalid vehicle count received from Python script'));
+            }
+          } else {
+            console.warn('Python script exited with code:', code);
+            reject(new Error('Python script error'));  
+          }
+      });
+    });
+  }
+
+module.exports.processimages = async (req , res) =>{
+    try {
+        const imagePaths = req.body;
+
+        if(imagePaths.length<3){
+            res.status(500).json({message:'Insufficient number of images'});    
+        }
+
+        const results=[{}];
+
+        for (const imagePath of imagePaths) {
+            try {
+                const vehicleCount = await callAI(imagePath.image);
+                results.push({vehicleCount : vehicleCount,side : imagePath.side});
+              } catch (error) {
+                console.error('Error processing image:', imagePath, error);
+              }
+        }
+        console.log(results);
+
+        let maxi=0;
+        let resultSide = "north";
+        for(const obj of results){
+          if(maxi<obj.vehicleCount){
+            resultSide = obj.side;
+            maxi=obj.vehicleCount;
+          }
+        }
+
+        res.status(200).json({message:"Images processed successfully", resultSide});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message:'Error processing images'});
+    }
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,166 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import DropImage from './Components/DropImage'
+import { useState, useEffect} from 'react';
+import Counter from './Components/Counter';
+import axios from 'axios';
+import Toast from './Components/Toast';
+
+export default function App() {
+  const [remainingTime , setRemainingTime ]= useState(60);
+  const [imageUrls, setImageUrls] = useState([]);
+  const [currentSide, setCurrentSide] = useState('north');
+  const [isPaused , setisPaused] = useState(false);
+  const [stackedside , setStackedSide] = useState(null);
+
+  function getNextSide(currentSide) {
+    switch (currentSide) {
+      case 'north':
+        return 'east';
+      case 'east':
+        return 'south';
+      case 'south':
+        return 'west';
+      case 'west':
+        return 'north';
+      default:
+        return 'north'; 
+    }
+  }
+
+  const checkCapturedImage = async () => {
+    if (!imageUrls || imageUrls.length === 0) {
+      return;
+    }
+    
+    const sideIndexMap = {
+      north: 0,
+      east: 1,
+      south: 2,
+      west: 3,
+    };
+
+    const sideMap = {
+      0: "north",
+      1: "east",
+      2: "south",
+      3: "west",
+    };
+
+    const currentSideImage = imageUrls[sideIndexMap[currentSide]];
+    if (currentSideImage && currentSideImage.includes('empty')) {
+      setisPaused(true);
+      setStackedSide(getNextSide(currentSide));
+      const restIndexes = [];
+      for(let i=0;i<=3;i++){
+        if(sideIndexMap[currentSide]!=i) restIndexes.push({image:imageUrls[i],side:sideMap[i]});
+      }
+      const response = await axios.post('http://localhost:3005/processimages' , restIndexes);
+      setCurrentSide(response.data.resultSide);
+      setisPaused(false);
+    }
+  };
+
+  
+
+  const fetchImageUrls = async () => {
+    try {
+      const response = await axios.get('http://localhost:3005/getimages');
+      const images = response.data.randomImages;
+      for (let i = images.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [images[i], images[j]] = [images[j], images[i]];
+      }
+      setImageUrls(images);
+     
+    } catch (error) {
+      console.error('Error fetching image URLs:', error);
+    }
+  };
+
+  useEffect(() => {
+    checkCapturedImage();
+  }, [imageUrls]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+     
+      if(!isPaused && remainingTime>0){
+        setRemainingTime(remainingTime-1);
+      }else if(remainingTime===0){
+        if(stackedside!=null){
+          setCurrentSide(stackedside);
+          setStackedSide(null);
+        }
+        else setCurrentSide(getNextSide(currentSide));
+        setRemainingTime(60);
+      }
+    }, 1000); 
+  
+    return () => clearInterval(intervalId);
+  }, [currentSide, remainingTime]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (remainingTime %10===0 && remainingTime>10) {
+        if(remainingTime==60 || stackedside==null) fetchImageUrls();
+      }
+    }, 1000); 
+  
+    return () => clearInterval(intervalId);
+  }, [currentSide, remainingTime]); 
+
+  return (
+    <>
+      <Navbar/>
+      <div className='absolute top-[7rem] right-[25rem]'>
+                <DropImage
+                  side="north"
+                  image={`./public/assets/${imageUrls[0]}`}
+                />
+                {console.log(imageUrls)}
+                <div className='flex justify-center'>
+                    {currentSide!="north"? <img src={`./public/assets/images/stop.png`} alt="" className=' w-1/2 h-20 ' />:<img src={`./public/assets/images/go.png`} alt="" className=' w-1/2 h-20 ' />}
+                </div>
+              
+     </div>
+      <div className='absolute top-[7rem] left-[22rem]'>
+              <DropImage
+                side="west"
+                image={`./public/assets/${imageUrls[3]}`}
+              />
+
+              <div className='flex justify-center'>
+              {currentSide!="west"? <img src={`./public/assets/images/stop.png`} alt="" className=' w-1/2 h-20 ' />:<img src={`./public/assets/images/go.png`} alt="" className=' w-1/2 h-20 ' />}
+              </div>
+              
+     </div>
+      <div className='absolute top-[20rem] left-[48rem]  '>
+        {isPaused?<Toast/>:
+          <Counter remainingTime={remainingTime}></Counter>}
+     </div>
+      <div className='absolute top-[29rem] left-[22rem]'>
+                <DropImage
+                  side="south"
+                  image={`./public/assets/${imageUrls[2]}`}
+                  
+                />
+                <div className='flex justify-center'>
+                {currentSide!="south"? <img src={`./public/assets/images/stop.png`} alt="" className=' w-1/2 h-20 ' />:<img src={`./public/assets/images/go.png`} alt="" className=' w-1/2 h-20 ' />}
+              </div>
+              
+     </div>
+      <div className='absolute top-[29rem] right-[25rem]'>
+                <DropImage
+                    side="east"
+                    image={`./public/assets/${imageUrls[1]}`}
+                    
+                />
+              <div className='flex justify-center'>
+              {currentSide!="east"? <img src={`./public/assets/images/stop.png`} alt="" className=' w-1/2 h-20 ' />:<img src={`./public/assets/images/go.png`} alt="" className=' w-1/2 h-20 ' />}
+              </div>
+ 
+      </div>
+    </>
+  )
+}
